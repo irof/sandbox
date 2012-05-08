2be330f31ac6d7169a87bf379ac722c89ecba9c6
irof <irof@javaring.info>
2012-05-09 01:40:01 +0900

    haskell fizzbuzz テストを演算子で書いてみる

--- fizzbuzz/FizzBuzz.hs
+++ fizzbuzz/FizzBuzz.hs
@@ -7,12 +7,12 @@ fizzBuzz i | i `mod` 5 == 0 = "Buzz"
 fizzBuzz i = show i
 
 tests =
-    TestCase (assertEqual "3でFizz"      "Fizz"     (fizzBuzz 3))
-  : TestCase (assertEqual "5でBuzz"      "Buzz"     (fizzBuzz 5))
-  : TestCase (assertEqual "6でFizz"      "Fizz"     (fizzBuzz 6))
-  : TestCase (assertEqual "1は1"         "1"        (fizzBuzz 1))
-  : TestCase (assertEqual "7は7"         "7"        (fizzBuzz 7))
-  : TestCase (assertEqual "15はFizzBuzz" "FizzBuzz" (fizzBuzz 15))
+    ("3 Fizz"      ~: "Fizz"     ~=? (fizzBuzz 3))
+  : ("5 Buzz"      ~: "Buzz"     ~=? (fizzBuzz 5))
+  : ("6 Fizz"      ~: "Fizz"     ~=? (fizzBuzz 6))
+  : ("1 1"         ~: "1"        ~=? (fizzBuzz 1))
+  : ("7 7"         ~: "7"        ~=? (fizzBuzz 7))
+  : ("15 FizzBuzz" ~: "FizzBuzz" ~=? (fizzBuzz 15))
   : []
 
 main = runTestTT $ TestList tests
