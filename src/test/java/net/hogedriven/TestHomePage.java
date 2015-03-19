473620268b229a0bff5c16af32b49064f2752c1c
irof <irof@hogedriven.net>
2015-03-19 23:16:13 +0900

    test refactor

--- src/test/java/net/hogedriven/TestHomePage.java
+++ src/test/java/net/hogedriven/TestHomePage.java
@@ -1,5 +1,6 @@
 package net.hogedriven;
 
+import net.hogedriven.i18n.SimplePage;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,30 +8,35 @@ import org.junit.Test;
 /**
  * Simple test using the WicketTester
  */
-public class TestHomePage
-{
-	private WicketTester tester;
+public class TestHomePage {
+    private WicketTester tester;
 
-	@Before
-	public void setUp()
-	{
-		tester = new WicketTester(new WicketApplication());
-	}
+    @Before
+    public void setUp() {
+        tester = new WicketTester(new WicketApplication());
+    }
 
-	@Test
-	public void homepageRendersSuccessfully()
-	{
-		//start and render the test page
-		tester.startPage(HomePage.class);
+    @Test
+    public void homepageRendersSuccessfully() {
+        //start and render the test page
+        tester.startPage(HomePage.class);
 
-		//assert rendered page class
-		tester.assertRenderedPage(HomePage.class);
-	}
+        //assert rendered page class
+        tester.assertRenderedPage(HomePage.class);
+    }
 
     @Test
     public void testVersion() {
         tester.startPage(HomePage.class);
 
         tester.assertLabel("version", "6.19.0");
+    }
+
+    @Test
+    public void linkHomePageToSimplePage() {
+        tester.startPage(HomePage.class);
+        tester.clickLink(tester.getComponentFromLastRenderedPage("simplePage"));
+
+        tester.assertRenderedPage(SimplePage.class);
     }
 }
--- src/test/java/net/hogedriven/i18n/SimplePageTest.java
+++ src/test/java/net/hogedriven/i18n/SimplePageTest.java
@@ -1,6 +1,5 @@
 package net.hogedriven.i18n;
 
-import net.hogedriven.HomePage;
 import net.hogedriven.WicketApplication;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
@@ -15,27 +14,16 @@ public class SimplePageTest {
     @Before
     public void setUp() {
         tester = new WicketTester(new WicketApplication());
-    }
-
-    @Test
-    public void linkHomePageToSimplePage() {
-        tester.startPage(HomePage.class);
-        tester.clickLink(tester.getComponentFromLastRenderedPage("simplePage"));
-
-        tester.assertRenderedPage(SimplePage.class);
+        tester.startPage(SimplePage.class);
     }
 
     @Test
     public void simplePropertyMessage() {
-        tester.startPage(SimplePage.class);
-
         tester.assertContains("hello simple property");
     }
 
     @Test
     public void simpleTagMessage() {
-        tester.startPage(SimplePage.class);
-
         tester.assertContains("hello tag attribute property");
     }
 }
