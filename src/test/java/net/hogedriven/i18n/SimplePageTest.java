d3f361708ca6a1fee4df47f859707e2674fce6b7
irof <irof@hogedriven.net>
2015-03-19 23:00:09 +0900

    simple property test

---
+++ src/test/java/net/hogedriven/i18n/SimplePageTest.java
@@ -0,0 +1,34 @@
+package net.hogedriven.i18n;
+
+import net.hogedriven.HomePage;
+import net.hogedriven.WicketApplication;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class SimplePageTest {
+    private WicketTester tester;
+
+    @Before
+    public void setUp() {
+        tester = new WicketTester(new WicketApplication());
+    }
+
+    @Test
+    public void linkHomePageToSimplePage() {
+        tester.startPage(HomePage.class);
+        tester.clickLink(tester.getComponentFromLastRenderedPage("simplePage"));
+
+        tester.assertRenderedPage(SimplePage.class);
+    }
+
+    @Test
+    public void simplePropertyMessage() {
+        tester.startPage(SimplePage.class);
+
+        tester.assertContains("hello simple property");
+    }
+}
