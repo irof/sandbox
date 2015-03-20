3b0d42747736352f78fc41770697d0a08c27d380
irof <irof@hogedriven.net>
2015-03-20 21:23:19 +0900

    refactor

--- src/test/java/net/hogedriven/components/SimplePageTest.java
+++ src/test/java/net/hogedriven/components/SimplePageTest.java
@@ -2,15 +2,21 @@ package net.hogedriven.components;
 
 import net.hogedriven.WicketApplication;
 import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SimplePageTest {
 
+    private WicketTester tester;
+
+    @Before
+    public void setup() {
+        tester = new WicketTester(new WicketApplication());
+        tester.startPage(SimplePage.class);
+    }
+
     @Test
     public void propertyField() {
-        WicketTester tester = new WicketTester(new WicketApplication());
-        tester.startPage(SimplePage.class);
-
         tester.assertLabel("property:field", "FIELD");
     }
 }
