8df6f8104f9572a2105bba6c00a7dfeb0625c469
irof <irof@hogedriven.net>
2015-03-22 18:42:42 +0900

    add structure package includes MyPage

---
+++ src/main/java/sandbox/structure/MyPage.java
@@ -0,0 +1,13 @@
+package sandbox.structure;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * @author irof
+ */
+public class MyPage extends WebPage {
+
+    public MyPage() {
+        super();
+    }
+}
---
+++ src/test/java/sandbox/structure/MyPageTest.java
@@ -0,0 +1,23 @@
+package sandbox.structure;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author irof
+ */
+public class MyPageTest {
+
+    private WicketTester tester;
+
+    @Before
+    public void setup() throws Exception {
+        tester = new WicketTester();
+    }
+
+    @Test
+    public void showPage() throws Exception {
+        tester.startPage(MyPage.class);
+    }
+}
