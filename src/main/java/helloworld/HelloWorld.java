e026e2263fa591b7e8b5a270240869624cdbc590
irof <irof@hogedriven.net>
2015-04-11 07:32:45 +0900

    remove main method

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -8,10 +8,6 @@ import javafx.stage.Stage;
 
 public class HelloWorld extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) {
         Button button = new Button();
