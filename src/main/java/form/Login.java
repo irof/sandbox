9532ac045024a24757f432589433606662bae46e
irof <irof@hogedriven.net>
2015-04-11 07:41:28 +0900

    Create a GridPane Layout

--- src/main/java/form/Login.java
+++ src/main/java/form/Login.java
@@ -1,6 +1,10 @@
 package form;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Login extends Application {
@@ -8,6 +12,14 @@ public class Login extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("JavaFX Welcome");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        primaryStage.setScene(new Scene(grid, 300, 275));
         primaryStage.show();
     }
 }
