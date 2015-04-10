c67b009c7652cccd673bb0cd2816d20341df43e5
irof <irof@hogedriven.net>
2015-04-11 08:03:49 +0900

    Add a Button and Text

--- src/main/java/form/Login.java
+++ src/main/java/form/Login.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -40,6 +42,15 @@ public class Login extends Application {
 
         PasswordField passwordField = new PasswordField();
         grid.add(passwordField, 1, 2);
+
+        Button button = new Button("Sign in");
+        HBox hBox = new HBox(10);
+        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+        hBox.getChildren().add(button);
+        grid.add(hBox, 1, 4);
+
+        Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
 
         primaryStage.setScene(new Scene(grid, 300, 275));
         primaryStage.show();
