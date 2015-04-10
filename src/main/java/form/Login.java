3e442eea820f6a960935bc4e74755aaeb079c44d
irof <irof@hogedriven.net>
2015-04-11 08:00:14 +0900

    Add Text, Labels, and TextFields

--- src/main/java/form/Login.java
+++ src/main/java/form/Login.java
@@ -4,7 +4,13 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Login extends Application {
@@ -18,6 +24,22 @@ public class Login extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0, 2);
+
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
 
         primaryStage.setScene(new Scene(grid, 300, 275));
         primaryStage.show();
