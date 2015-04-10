5d5b7af2d31f7d4b0a8700a548809b783e096821
irof <irof@hogedriven.net>
2015-04-11 07:38:27 +0900

    4. Creationg Form in JavaFX

---
+++ src/main/java/form/Login.java
@@ -0,0 +1,13 @@
+package form;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Welcome");
+        primaryStage.show();
+    }
+}
