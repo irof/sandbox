0d97c3bde9e16ae8829f27b02d24226e23ddc9a4
irof <irof@hogedriven.net>
2015-04-29 13:59:35 +0900

    Jerseyを使って見る

---
+++ jaxrs/jersey/src/main/java/sample/Station.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+/**
+ * @author irof
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Station {
+    private final String number;
+    private final String name;
+
+    /**
+     * @deprecated JAXBContextとかが使う
+     */
+    @Deprecated
+    public Station() {
+        this(null, null);
+    }
+
+    public Station(String number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return number + ":" + name;
+    }
+}
