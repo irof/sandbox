35f0ab6b19f28eab4bb8487e90aa3a1e325fa8fd
irof <irof@hogedriven.net>
2015-05-01 05:30:50 +0900

    refactor

--- jaxrs/jersey/src/main/java/sample/Kitakyu.java
+++ jaxrs/jersey/src/main/java/sample/Kitakyu.java
@@ -5,12 +5,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Path("kitakyu")
 public class Kitakyu {
@@ -18,12 +17,11 @@ public class Kitakyu {
     static final Map<String, Station> stations;
 
     static {
-        stations = Arrays.asList(
+        stations = Stream.of(
                 new Station("M08", "千里中央"),
                 new Station("M09", "桃山台"),
                 new Station("M10", "緑地公園"),
                 new Station("M11", "江坂"))
-                .stream()
                 .collect(Collectors.toMap(Station::getNumber, Function.identity()));
     }
 
