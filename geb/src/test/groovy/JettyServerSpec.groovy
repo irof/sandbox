c7b0e7c6509584be0287330299d8ea2c063a6dbc
irof <irof@hogedriven.net>
2015-04-30 13:19:58 +0900

    fix

--- geb/src/test/groovy/JettyServerSpec.groovy
+++ geb/src/test/groovy/JettyServerSpec.groovy
@@ -1,44 +1,38 @@
 import com.github.mjeanroy.junit.servers.jetty.EmbeddedJetty
 import com.github.mjeanroy.junit.servers.rules.JettyServerRule
-import org.junit.Before
+import geb.spock.GebSpec
 import org.junit.ClassRule
-import org.junit.Test
+import spock.lang.Shared
 
-class JettyServerSpec {
+class JettyServerSpec extends GebSpec {
 
-    @ClassRule
-    public static JettyServerRule server = new JettyServerRule(new EmbeddedJetty());
+    @Shared @ClassRule
+    JettyServerRule server = new JettyServerRule(new EmbeddedJetty());
 
-    @Before
-    void setup() {
+    def setup() {
         go "http://localhost:${server.port}"
     }
 
-    @Test
     def "indexを表示する"() {
         expect:
         title == "JettyServerRuleIndexPage"
     }
 
-    @Test
     def "テキストの内容で選り分ける_HtmlUnit"() {
         expect:
         $("section.classA div", text: ~/.*ふが.*/).$("p").text() == "取りたいやつ"
     }
 
-    @Test
     def "テキストの内容で選り分ける"() {
         expect:
         $("section.classA div").has("span", text: ~/.*ふが.*/).$("p").text() == "取りたいやつ"
     }
 
-    @Test
     def "表示されているかで選り分ける_HtmlUnit"() {
         expect:
         $("section.classB div").findAll { !it.attr("style").contains("display:none") }.$("p").text() == "取りたいやつ"
     }
 
-    @Test
     def "表示されているかで選り分ける_FireFox"() {
         expect:
         $("section.classB div").findAll { it.displayed }.$("p").text() == "取りたいやつ"
