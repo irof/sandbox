e6357daa1a1414a1c5cfb553be3107694cc14d58
irof <irof@hogedriven.net>
2015-05-02 20:40:09 +0900

    整理

--- junit/src/test/java/pracswing/v4_12/PracAnnotationValidator.java
+++ junit/src/test/java/pracswing/v4_12/PracAnnotationValidator.java
@@ -1,21 +1,23 @@
 package pracswing.v4_12;
 
 import org.junit.Test;
+import org.junit.runners.model.TestClass;
+import org.junit.validator.AnnotationValidator;
 import org.junit.validator.ValidateWith;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import static org.junit.Assume.assumeThat;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * {@link org.junit.validator.AnnotationValidator} を無理矢理使ってみる。
  *
  * @author irof
  */
-@MyAnnotation
+@PracAnnotationValidator.MyAnnotation
 public class PracAnnotationValidator {
 
     // コメント外したらAnnotationValidatorで設定した例外が出てくる
@@ -24,11 +26,24 @@ public class PracAnnotationValidator {
     @Test
     public void hoge() throws Exception {
     }
-}
 
-@Retention(RetentionPolicy.RUNTIME)
-@ValidateWith(NoFieldValidator.class)
-@Target(ElementType.TYPE)
-@interface MyAnnotation {
-}
+    /**
+     * テストクラスがFieldを持っていけないと言う謎のValidator
+     */
+    public static class NoFieldValidator extends AnnotationValidator {
 
+        @Override
+        public List<Exception> validateAnnotatedClass(TestClass testClass) {
+            if (testClass.getJavaClass().getDeclaredFields().length > 0) {
+                return Collections.singletonList(new Exception("フィールドがあるよ？"));
+            }
+            return super.validateAnnotatedClass(testClass);
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @ValidateWith(NoFieldValidator.class)
+    @Target(ElementType.TYPE)
+    @interface MyAnnotation {
+    }
+}
