bea46c78dcc9557cd6c177510e8996e1179e8c25
irof <irof@hogedriven.net>
2015-05-10 19:22:57 +0900

    LinkをBookmarkablePageLinkに変更

--- wicket/src/main/java/sandbox/HomePage.java
+++ wicket/src/main/java/sandbox/HomePage.java
@@ -1,6 +1,7 @@
 package sandbox;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import sandbox.i18n.ResourcePage;
@@ -11,45 +12,10 @@ public class HomePage extends WebPage {
     public HomePage(final PageParameters parameters) {
         super(parameters);
 
-        add(new Link<Void>("i18n.link") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(ResourcePage.class);
-            }
-        });
-
-        add(new Link<Void>("misc.link") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(sandbox.misc.SimplePage.class);
-            }
-        });
-
-        add(new Link<Void>("list.link") {
-
-            @Override
-            public void onClick() {
-
-                setResponsePage(sandbox.list.ListPage.class);
-            }
-        });
-
-        add(new Link<Void>("structure.link") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(sandbox.structure.MyPage.class);
-            }
-        });
-
-        add(new Link<Void>("model.link") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(sandbox.model.SimplePage.class);
-            }
-        });
+        add(new BookmarkablePageLink<>("i18n.link", ResourcePage.class));
+        add(new BookmarkablePageLink<>("misc.link", sandbox.misc.SimplePage.class));
+        add(new BookmarkablePageLink<>("list.link", sandbox.list.ListPage.class));
+        add(new BookmarkablePageLink<>("structure.link", sandbox.structure.MyPage.class));
+        add(new BookmarkablePageLink<>("model.link", sandbox.model.SimplePage.class));
     }
 }
