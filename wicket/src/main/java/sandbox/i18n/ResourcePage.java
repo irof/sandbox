dc386c33829975f374c4079909e961403416558c
irof <irof@hogedriven.net>
2015-05-10 22:33:10 +0900

    サンプルっぽく整理

--- wicket/src/main/java/sandbox/i18n/ResourcePage.java
+++ wicket/src/main/java/sandbox/i18n/ResourcePage.java
@@ -2,12 +2,13 @@ package sandbox.i18n;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.model.StringResourceModel;
-import org.apache.wicket.model.util.MapModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResourcePage extends WebPage {
     private static final long serialVersionUID = 1L;
@@ -15,11 +16,15 @@ public class ResourcePage extends WebPage {
     public ResourcePage(final PageParameters parameters) {
         super(parameters);
 
-        add(new Label("resource.static", new ResourceModel("hello.message3")));
-        add(new Label("resource.dynamic", new StringResourceModel("hello.message4", this, null, null, "Dynamic")));
+        add(new Label("resource.static", new ResourceModel("simple")));
 
-        add(new Label("hello", new StringResourceModel("hello.message5", this,
-                new MapModel<String, String>(Collections.singletonMap("name", "irof")))));
+        Object[] args = {"Dynamic", "Args"};
+        add(new Label("resource.dynamic1", new StringResourceModel("dynamic1", Model.of(), args)));
+
+        Map<String, String> valueMap = new HashMap<>();
+        valueMap.put("arg1", "Argument1");
+        valueMap.put("arg3", "Argument3");
+        add(new Label("resource.dynamic2", new StringResourceModel("dynamic2", Model.ofMap(valueMap))));
 
         add(new Label("markup.label.pattern1", new ResourceModel("markup.pattern1")));
         add(new Label("markup.label.pattern2", new ResourceModel("markup.pattern2")));
--- wicket/src/test/java/sandbox/i18n/ResourcePageTest.java
+++ wicket/src/test/java/sandbox/i18n/ResourcePageTest.java
@@ -5,6 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import sandbox.WicketApplication;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Simple test using the WicketTester
  */
@@ -18,27 +21,29 @@ public class ResourcePageTest {
     }
 
     @Test
-    public void simplePropertyMessage() {
-        tester.assertContains("hello simple property");
+    public void Javaファイルを介しない_wicketMessageタグ() {
+        assertThat(tester.getTagById("simple1").getValue(), is("シンプルなメッセージ"));
     }
 
     @Test
-    public void simpleTagMessage() {
-        tester.assertContains("hello tag attribute property");
+    public void Javaファイルを介しない_inputタグ() {
+        // <input id="simple2" value="シンプルなメッセージ"/>
+        String value = tester.getTagById("simple2").getAttribute("value");
+        assertThat(value, is("シンプルなメッセージ"));
     }
 
     @Test
-    public void resourceModelMessageStatic() {
-        tester.assertLabel("resource.static", "Resource Model message");
+    public void ResourceModel() {
+        tester.assertLabel("resource.static", "シンプルなメッセージ");
     }
 
     @Test
-    public void resourceModelMessageDynamic() {
-        tester.assertLabel("resource.dynamic", "Dynamic Resource Model message");
+    public void StringResourceModel1() {
+        tester.assertLabel("resource.dynamic1", "動的なメッセージ1[Dynamic, Args, {2}]");
     }
 
     @Test
-    public void helloWorld() {
-        tester.assertLabel("hello", "hello irof.");
+    public void StringResourceModel2() {
+        tester.assertLabel("resource.dynamic2", "動的なメッセージ2[Argument1, ${arg2}, Argument3]");
     }
 }
