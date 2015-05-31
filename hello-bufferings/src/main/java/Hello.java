b483cb6769a356c0d5ce8dd0f035e58a1cb24d21
irof <irof@hogedriven.net>
2015-05-31 23:54:51 +0900

    if elseのまーじ

--- hello-bufferings/src/main/java/Hello.java
+++ hello-bufferings/src/main/java/Hello.java
@@ -19,55 +19,53 @@ public class Hello {
         try {
             if (in.getSelected() == null || in.getSelected() <= 0 || in.getSelected() >= 11) {
                 throw new IllegalArgumentException();
+            } else if (in.getCoins() == null || in.getCoins().size() <= 0 || in.getCoins().size() >= 101) {
+                throw new IllegalArgumentException();
             } else {
-                if (in.getCoins() == null || in.getCoins().size() <= 0 || in.getCoins().size() >= 101) {
-                    throw new IllegalArgumentException();
+                sum = 0;
+                for (Integer coin : in.getCoins()) {
+                    if (coin == 1) {
+                        sum += 0;
+                        coins2.add(coin);
+                    } else if (coin == 5) {
+                        sum += 0;
+                        coins2.add(coin);
+                    } else if (coin == 10) {
+                        sum += 10;
+                    } else if (coin == 50) {
+                        sum += 50;
+                    } else if (coin == 100) {
+                        sum += 100;
+                    } else if (coin == 500) {
+                        sum += 500;
+                    }
+                }
+
+                Product product = dao.findById(in.getSelected());
+                if (product == null) {
+                    result = new Result();
+                    result.setCoins(in.getCoins());
+                    result.setProduct(null);
+                } else if (product.getPrice() <= sum) {
+                    result.setProduct(product);
+                    sum -= product.getPrice();
+
+                    List<Integer> coins3 = new ArrayList<>();
+                    coins3.add(500);
+                    coins3.add(100);
+                    coins3.add(50);
+                    coins3.add(10);
+                    for (Integer coin : coins3) {
+                        while (sum >= coin) {
+                            coins2.add(coin);
+                            sum -= coin;
+                        }
+                    }
+                    result.setCoins(coins2);
                 } else {
-                    sum = 0;
-                    for (Integer coin : in.getCoins()) {
-                        if (coin == 1) {
-                            sum += 0;
-                            coins2.add(coin);
-                        } else if (coin == 5) {
-                            sum += 0;
-                            coins2.add(coin);
-                        } else if (coin == 10) {
-                            sum += 10;
-                        } else if (coin == 50) {
-                            sum += 50;
-                        } else if (coin == 100) {
-                            sum += 100;
-                        } else if (coin == 500) {
-                            sum += 500;
-                        }
-                    }
-
-                    Product product = dao.findById(in.getSelected());
-                    if (product == null) {
-                        result = new Result();
-                        result.setCoins(in.getCoins());
-                        result.setProduct(null);
-                    } else if (product.getPrice() <= sum) {
-                        result.setProduct(product);
-                        sum -= product.getPrice();
-
-                        List<Integer> coins3 = new ArrayList<>();
-                        coins3.add(500);
-                        coins3.add(100);
-                        coins3.add(50);
-                        coins3.add(10);
-                        for (Integer coin : coins3) {
-                            while (sum >= coin) {
-                                coins2.add(coin);
-                                sum -= coin;
-                            }
-                        }
-                        result.setCoins(coins2);
-                    } else {
-                        result = new Result();
-                        result.setCoins(in.getCoins());
-                        result.setProduct(null);
-                    }
+                    result = new Result();
+                    result.setCoins(in.getCoins());
+                    result.setProduct(null);
                 }
             }
         } catch (Exception e) {
