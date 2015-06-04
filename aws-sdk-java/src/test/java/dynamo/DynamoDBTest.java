53beb2b8cd3a210aa03fbdfea9f90c237144d029
irof <irof@hogedriven.net>
2015-06-04 20:52:04 +0900

    DynamoDBのtable作ったり消したり

--- aws-sdk-java/src/test/java/dynamo/DynamoDBTest.java
+++ aws-sdk-java/src/test/java/dynamo/DynamoDBTest.java
@@ -1,9 +1,14 @@
 package dynamo;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.amazonaws.services.dynamodbv2.util.Tables;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Stopwatch;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -14,10 +19,45 @@ import static org.junit.Assert.assertThat;
  */
 public class DynamoDBTest {
 
+    private AmazonDynamoDBClient dynamoDB;
+
+    @Rule
+    public Stopwatch stopwatch = new Stopwatch() {
+    };
+
+    @Before
+    public void setup() throws Exception {
+        dynamoDB = new AmazonDynamoDBClient();
+    }
+
     @Test
     public void listTables() throws Exception {
-        AmazonDynamoDB dynamoDB = new AmazonDynamoDBClient();
         ListTablesResult tables = dynamoDB.listTables();
         assertThat(tables.getTableNames(), is(notNullValue()));
+    }
+
+    @Test
+    public void tableCreateAndDelete() throws Exception {
+        assertThat(Tables.doesTableExist(dynamoDB, "myTable"), is(false));
+        System.out.println(stopwatch.runtime(TimeUnit.MILLISECONDS));
+
+        CreateTableRequest tableRequest = new CreateTableRequest()
+                .withTableName("myTable")
+                .withKeySchema(new KeySchemaElement("myName", KeyType.HASH))
+                .withAttributeDefinitions(new AttributeDefinition("myName", ScalarAttributeType.S))
+                .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+        CreateTableResult createResult = dynamoDB.createTable(tableRequest);
+        System.out.println(stopwatch.runtime(TimeUnit.MILLISECONDS));
+
+        // 20秒くらいかかる
+        Tables.awaitTableToBecomeActive(dynamoDB, "myTable");
+        assertThat(Tables.doesTableExist(dynamoDB, "myTable"), is(true));
+        System.out.println(stopwatch.runtime(TimeUnit.MILLISECONDS));
+
+        DeleteTableResult deleteResult = dynamoDB.deleteTable("myTable");
+        System.out.println(stopwatch.runtime(TimeUnit.MILLISECONDS));
+
+        assertThat(Tables.doesTableExist(dynamoDB, "myTable"), is(false));
+        System.out.println(stopwatch.runtime(TimeUnit.MILLISECONDS));
     }
 }
