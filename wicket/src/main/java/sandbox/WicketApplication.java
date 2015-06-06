d6a30a2567083d9d40c94cb1102c21f942ab9dea
irof <irof@hogedriven.net>
2015-06-06 23:02:17 +0900

    springに関係あるのを寄せとく

--- wicket/src/main/java/sandbox/WicketApplication.java
+++ wicket/src/main/java/sandbox/WicketApplication.java
@@ -4,6 +4,7 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -20,8 +21,11 @@ public class WicketApplication extends WebApplication {
 
         // WicketからSpringを使う程度ならこのタイミングで作るのがシンプルっぽい。設定もここだけでいいしね。
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.getEnvironment().addActiveProfile("def");
-        ctx.register(Config.class);
+        // とりあえず2つアクティブにしとく
+        ctx.getEnvironment().addActiveProfile("active1");
+        ctx.getEnvironment().addActiveProfile("active2");
+        // Springで遊ぶパッケージ
+        ctx.scan("sandbox.spring");
         ctx.refresh();
 
         // Wicketのコンポーネント生成時に @SpringBean にプロキシをインジェクションするリスナー。
@@ -31,26 +35,5 @@ public class WicketApplication extends WebApplication {
         // SpringWebApplicationFactory を WicketFilter に設定するのは WebApplication をSpring管理下にしたいとき。
         // その際の ApplicationContext の生成は外でやる。
         // Wicketの外でSpring使う時は ContextLoaderListener とか AbstractContextLoaderInitializer を実装する。
-    }
-
-    @Configuration
-    public static class Config {
-
-        @Profile("abc")
-        @Bean(name = "hoge")
-        public String str1() {
-            return "HOGE";
-        }
-
-        @Profile("!abc")
-        @Bean(name = "hoge")
-        public String str2() {
-            return "hogehoge";
-        }
-
-        @Bean(name = "fuga")
-        public String str3() {
-            return "FUGA";
-        }
     }
 }
--- wicket/src/main/java/sandbox/spring/SpringPage.java
+++ wicket/src/main/java/sandbox/spring/SpringPage.java
@@ -3,6 +3,9 @@ package sandbox.spring;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author irof
@@ -22,5 +25,26 @@ public class SpringPage extends WebPage {
         add(new Label("profile", "profile"));
         add(new Label("text1", a));
         add(new Label("text2", b));
+    }
+
+    @Configuration
+    public static class Config {
+
+        @Profile("active1")
+        @Bean(name = "hoge")
+        public String str1() {
+            return "active1が有効な時のhoge";
+        }
+
+        @Profile("!active1")
+        @Bean(name = "hoge")
+        public String str2() {
+            return "active1が無効な時のhoge";
+        }
+
+        @Bean(name = "fuga")
+        public String str3() {
+            return "profileに関係ないfuga";
+        }
     }
 }
