9724481248a7db0595b2066653a2ca35fe81907d
irof <irof@hogedriven.net>
2015-06-11 10:17:35 +0900

    AmazonHttpClientの動きを確認するやつ

---
+++ aws-sdk-java/src/test/java/core/AmazonHttpClientTest.java
@@ -0,0 +1,104 @@
+package core;
+
+import com.amazonaws.*;
+import com.amazonaws.http.AmazonHttpClient;
+import com.amazonaws.http.ExecutionContext;
+import com.amazonaws.http.HttpResponse;
+import com.amazonaws.http.HttpResponseHandler;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.TemporaryServer;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+
+/**
+ * @author irof
+ */
+public class AmazonHttpClientTest {
+
+    private final static Logger logger = LoggerFactory.getLogger("test");
+
+    @Rule
+    public TemporaryServer server = new TemporaryServer()
+            .withSleep(2)
+            .withBody(() -> "HOGEFUGAPIYO");
+
+    @Test
+    public void 同時接続数はAmazonHttpClientのインスタンス単位だよなぁという確認() throws Exception {
+        ClientConfiguration config = new ClientConfiguration();
+        // 同時接続数
+        config.setMaxConnections(2);
+        // リトライしない
+        config.setMaxErrorRetry(0);
+        // タイムアウトの時間
+        config.setSocketTimeout(5_000);
+
+        Supplier<AmazonHttpClient> client = clientSupplier(config);
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<Future<Void>> futures = executor.invokeAll(Arrays.asList(
+                submit(client),
+                submit(client),
+                submit(client),
+                submit(client)
+        ));
+
+        // 全部終わるの待つ
+        for (Future<Void> future : futures) {
+            future.get();
+        }
+    }
+
+    /**
+     * @param config 設定
+     * @return テストで使用するクライアント提供オブジェクト
+     */
+    private Supplier<AmazonHttpClient> clientSupplier(ClientConfiguration config) {
+        // 毎回クライアントを作るやつ
+        //Supplier<AmazonHttpClient> client = () -> new AmazonHttpClient(config);
+
+        // 一度作ったクライアントを共有するやつ
+        AmazonHttpClient httpClient = new AmazonHttpClient(config);
+        return () -> httpClient;
+    }
+
+    private Callable<Void> submit(Supplier<AmazonHttpClient> client) {
+        return () -> {
+            Request<?> request = new DefaultRequest<>("test-service");
+            request.setEndpoint(server.getUri());
+            InputStream in = new ByteArrayInputStream("hoge".getBytes(StandardCharsets.UTF_8));
+            request.setContent(in);
+
+            HttpResponseHandler<AmazonServiceException> errorResponse = null;
+            ExecutionContext context = new ExecutionContext();
+
+            client.get().execute(request, responseHandler(), errorResponse, context);
+            return null;
+        };
+    }
+
+    private HttpResponseHandler<AmazonWebServiceResponse<Object>> responseHandler() {
+        return new HttpResponseHandler<AmazonWebServiceResponse<Object>>() {
+            @Override
+            public AmazonWebServiceResponse<Object> handle(HttpResponse response) throws Exception {
+                return new AmazonWebServiceResponse<>();
+            }
+
+            @Override
+            public boolean needsConnectionLeftOpen() {
+                return false;
+            }
+        };
+    }
+}
---
+++ aws-sdk-java/src/test/java/util/TemporaryServer.java
@@ -0,0 +1,106 @@
+package util;
+
+import org.junit.rules.ExternalResource;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+/**
+ * @author irof
+ */
+public class TemporaryServer extends ExternalResource {
+
+    private static final Logger logger = Logger.getLogger(TemporaryServer.class.getName());
+
+    private ServerSocket socket;
+    private ExecutorService service;
+    private int seconds = 0;
+    private Supplier<String> bodySupplier = () -> "DUMMY";
+
+    public TemporaryServer withSleep(int seconds) {
+        this.seconds = seconds;
+        return this;
+    }
+
+    public TemporaryServer withBody(Supplier<String> bodySupplier) {
+        this.bodySupplier = bodySupplier;
+        return this;
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        // 空いてるポートで作る
+        socket = new ServerSocket(0);
+        service = Executors.newSingleThreadExecutor();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss zzz", Locale.US);
+
+        service.submit(() -> {
+            while (!socket.isClosed()) {
+
+                try (Socket s = socket.accept()) {
+                    TimeUnit.SECONDS.sleep(seconds);
+
+                    String body = bodySupplier.get();
+                    OutputStream outputStream = s.getOutputStream();
+                    OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+                    writer.write("HTTP/1.1 200 OK\n");
+
+                    writer.write("Connection: close\n");
+                    writer.write("Date: " + ZonedDateTime.now(ZoneId.of("GMT")).format(formatter) + "\n");
+
+                    writer.write("Server: " + this.getClass().getName() + "\n");
+
+                    writer.write("Content-Length: " + body.getBytes(StandardCharsets.UTF_8).length + "\n");
+                    writer.write("Content-Type: text/plain; charset=UTF-8\n");
+
+                    writer.write("\n");
+
+                    writer.write(body);
+                    writer.flush();
+                    outputStream.flush();
+                }
+            }
+            // Callableにするため
+            return null;
+        });
+    }
+
+    @Override
+    protected void after() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            logger.severe(e.toString());
+        }
+        service.shutdown();
+    }
+
+    public int getPort() {
+        return socket.getLocalPort();
+    }
+
+    public URI getUri() {
+        try {
+            return new URI("http://localhost:" + getPort());
+        } catch (URISyntaxException e) {
+            logger.severe(e.toString());
+            throw new AssertionError(e);
+        }
+    }
+}
