d41cf99455eee9155a914aaa3fcd13ed69b014d0
irof <irof@hogedriven.net>
2015-06-15 22:51:13 +0900

    LabelとTextFieldを適当に切り替えるの

--- wicket/src/main/java/sandbox/misc/SimplePage.java
+++ wicket/src/main/java/sandbox/misc/SimplePage.java
@@ -1,7 +1,13 @@
 package sandbox.misc;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import sandbox.misc.switching.LabelOrTextField;
 
 public class SimplePage extends WebPage {
     private static final long serialVersionUID = 1L;
@@ -12,5 +18,9 @@ public class SimplePage extends WebPage {
         add(new ButtonPanel("panel.button"));
         add(new AjaxButtonPanel("panel.button.ajax"));
         add(new ListPanel("panel.list"));
+
+        Model<String> hoge1 = Model.of("hogehoge");
+        add(new LabelOrTextField("panel.switching.input", hoge1, Model.of(true)));
+        add(new LabelOrTextField("panel.switching.label", Model.of("fugafuga"), Model.of(false)));
     }
 }
---
+++ wicket/src/main/java/sandbox/misc/switching/LabelOrTextField.java
@@ -0,0 +1,26 @@
+package sandbox.misc.switching;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+public class LabelOrTextField extends Panel {
+
+    public LabelOrTextField(String id, Model<String> model, Model<Boolean> canEdit) {
+        super(id);
+
+        add(new Label("label", model) {
+            @Override
+            protected void onConfigure() {
+                setVisibilityAllowed(!canEdit.getObject());
+            }
+        });
+        add(new TextField<String>("textField", model) {
+            @Override
+            protected void onConfigure() {
+                setVisibilityAllowed(canEdit.getObject());
+            }
+        });
+    }
+}
