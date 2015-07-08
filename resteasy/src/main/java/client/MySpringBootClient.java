68168547357f0625f9d9efffd64df96c0c32bcee
irof <irof@hogedriven.net>
2015-07-09 07:48:38 +0900

    httpsにしてみる

--- resteasy/src/main/java/client/MySpringBootClient.java
+++ resteasy/src/main/java/client/MySpringBootClient.java
@@ -2,15 +2,28 @@ package client;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
 
 /**
  * @author irof
  */
 public class MySpringBootClient {
 
-    public static void main(String[] args) {
-        Response response = ClientBuilder.newClient()
-                .target("http://localhost:9000")
+    public static void main(String[] args) throws Exception {
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+
+        // 動くけどなんか違う気がする
+        KeyStore trustStore = KeyStore.getInstance("JKS", "SUN");
+        try (InputStream inputStream = Files.newInputStream(Paths.get("hoge.keystore"))) {
+            trustStore.load(inputStream, "hogehoge".toCharArray());
+        }
+        clientBuilder.trustStore(trustStore);
+
+        Response response = clientBuilder.build()
+                .target("https://localhost:9000")
                 .path("hello")
                 .request()
                 .get();
