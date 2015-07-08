ac7774000d60ddf338e94a8075b10438795d2fac
irof <irof@hogedriven.net>
2015-07-09 08:54:45 +0900

    hostnameを検証しない

--- resteasy/src/main/java/client/MySpringBootClient.java
+++ resteasy/src/main/java/client/MySpringBootClient.java
@@ -1,5 +1,6 @@
 package client;
 
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
@@ -13,16 +14,19 @@ import java.security.KeyStore;
 public class MySpringBootClient {
 
     public static void main(String[] args) throws Exception {
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
 
         // 動くけどなんか違う気がする
         KeyStore trustStore = KeyStore.getInstance("JKS", "SUN");
         try (InputStream inputStream = Files.newInputStream(Paths.get("hoge.keystore"))) {
             trustStore.load(inputStream, "hogehoge".toCharArray());
         }
-        clientBuilder.trustStore(trustStore);
+        Client client = ClientBuilder.newBuilder()
+                .trustStore(trustStore)
+                // hostnameの検証を無効にするー
+                .hostnameVerifier((hostname, session) -> true)
+                .build();
 
-        Response response = clientBuilder.build()
+        Response response = client
                 .target("https://localhost:9000")
                 .path("hello")
                 .request()
