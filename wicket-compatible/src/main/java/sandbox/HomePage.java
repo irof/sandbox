f47ec0ae81675c051eb3481a202c17ed6c730b0a
irof <irof@hogedriven.net>
2015-07-09 23:35:44 +0900

    wicketのバージョンごとの挙動確認するやつ

---
+++ wicket-compatible/src/main/java/sandbox/HomePage.java
@@ -0,0 +1,30 @@
+package sandbox;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+/**
+ * @author irof
+ */
+public class HomePage extends WebPage {
+
+    IModel<String> model = Model.of("hoge");
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        Form<Void> form = new Form<Void>("form") {
+            @Override
+            protected void onSubmit() {
+                model.setObject("fuga");
+            }
+        };
+
+        form.add(new Label("message", model));
+        add(form);
+    }
+}
---
+++ wicket-compatible/src/main/java/sandbox/WicketApplication.java
@@ -0,0 +1,16 @@
+package sandbox;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class WicketApplication extends WebApplication {
+    @Override
+    public Class<? extends WebPage> getHomePage() {
+        return HomePage.class;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+}
