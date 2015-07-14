602d4925815b5c500eeb7b9a471c342e2a5928bd
irof <irof@hogedriven.net>
2015-07-15 08:54:10 +0900

    urlとかcontextPathとかとってみる

--- wicket/src/main/java/sandbox/misc/MiscPage.java
+++ wicket/src/main/java/sandbox/misc/MiscPage.java
@@ -1,7 +1,12 @@
 package sandbox.misc;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.cycle.RequestCycle;
+import sandbox.HomePage;
 import sandbox.misc.switching.LabelOrTextField;
 
 /**
@@ -22,5 +27,15 @@ public class MiscPage extends WebPage {
         Model<String> hoge1 = Model.of("hogehoge");
         add(new LabelOrTextField("panel.switching.input", hoge1, Model.of(true)));
         add(new LabelOrTextField("panel.switching.label", Model.of("fugafuga"), Model.of(false)));
+
+        // "http://..../wicket"
+        add(new Label("fullUrl",
+                RequestCycle.get().getUrlRenderer().renderFullUrl(
+                        Url.parse(urlFor(HomePage.class, null).toString()))));
+        // "/wicket"
+        add(new Label("contextPath",
+                WebApplication.get().getServletContext().getContextPath()));
+        // "../../" 相対位置になる
+        add(new Label("urlForPage", urlFor(HomePage.class, null).toString()));
     }
 }
