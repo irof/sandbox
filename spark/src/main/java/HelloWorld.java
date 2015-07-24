9bca3b515c0c8fa5a76c67dc113ad59ae0df1a47
irof <irof@hogedriven.net>
2015-07-24 21:45:52 +0900

    適当にリクエストの中身出したりしてみる

--- spark/src/main/java/HelloWorld.java
+++ spark/src/main/java/HelloWorld.java
@@ -1,10 +1,44 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 import static spark.Spark.get;
 
 /**
  * @author irof
  */
 public class HelloWorld {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorld.class);
+
     public static void main(String[] args) {
+
         get("/hello", (req, res) -> "Hello World");
+
+        get("/sample/status/:code", (req, res) -> {
+            res.status(Integer.valueOf(req.params(":code")));
+            return "HTTP " + req.params(":code");
+        });
+
+        get("/loggingRequest", (req, res) -> {
+            LOG.info("request : {}", req);
+
+            Arrays.stream(req.getClass().getDeclaredMethods())
+                    .filter(method -> method.getParameterCount() == 0)
+                    .forEach(method -> {
+                        try {
+                            method.setAccessible(true);
+                            LOG.info("request.{}() : {}", method.getName(), method.invoke(req));
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+
+            return req.toString();
+        });
     }
 }
