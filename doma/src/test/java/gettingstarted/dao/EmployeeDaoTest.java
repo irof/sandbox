c1c009f811535c4cb859a7f15579b11dea4d560f
irof <irof@hogedriven.net>
2015-09-23 23:51:49 +0900

    登録更新削除ー

--- doma/src/test/java/gettingstarted/dao/EmployeeDaoTest.java
+++ doma/src/test/java/gettingstarted/dao/EmployeeDaoTest.java
@@ -5,6 +5,7 @@ import gettingstarted.entity.Employee;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExternalResource;
+import org.seasar.doma.jdbc.tx.LocalTransactionManager;
 import org.seasar.doma.jdbc.tx.TransactionManager;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class EmployeeDaoTest {
     @Rule
     public ExternalResource dbResource = new ExternalResource() {
 
-        AppDao dao  = new AppDaoImpl();
+        AppDao dao = new AppDaoImpl();
 
         @Override
         protected void before() throws Throwable {
@@ -46,6 +47,52 @@ public class EmployeeDaoTest {
         AppConfig.singleton().getTransactionManager().required(() -> {
             List<Employee> employees = dao.selectByAge(32);
             assertThat(employees, hasSize(1));
+        });
+    }
+
+    @Test
+    public void 登録更新削除() throws Exception {
+        LocalTransactionManager tm = AppConfig.singleton().getTransactionManager();
+
+        Employee hoge = new Employee();
+        hoge.age = 32;
+        hoge.name = "HOGE";
+
+        assertThat(hoge.id, is(nullValue()));
+
+        // INSERT
+        tm.required(() -> {
+            dao.insert(hoge);
+            assertThat(hoge.id, is(notNullValue()));
+        });
+        tm.required(() -> {
+            Employee employee = dao.selectById(hoge.id);
+            assertThat(employee.name, is("HOGE"));
+        });
+
+        // UPDATE
+        tm.required(() -> {
+            Employee employee = dao.selectById(hoge.id);
+            assertThat(employee.age, is(32));
+
+            employee.age = 33;
+            int update = dao.update(employee);
+            assertThat(update, is(1));
+        });
+        tm.required(() -> {
+            Employee employee = dao.selectById(hoge.id);
+            assertThat(employee.age, is(33));
+        });
+
+        // DELETE
+        tm.required(() -> {
+            Employee employee = dao.selectById(hoge.id);
+            int delete = dao.delete(employee);
+            assertThat(delete, is(1));
+        });
+        tm.required(() -> {
+            Employee employee = dao.selectById(hoge.id);
+            assertThat(employee, is(nullValue()));
         });
     }
 }
