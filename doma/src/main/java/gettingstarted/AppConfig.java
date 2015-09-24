3832c97df5b2c035fd3cc900e4e06305df37f246
irof <irof@hogedriven.net>
2015-09-24 23:50:56 +0900

    logbackでログ出してみようとしたけどlogbackよくわかってなかった
    jul-to-slf4jでいいんじゃないかと思わなくはない

--- doma/src/main/java/gettingstarted/AppConfig.java
+++ doma/src/main/java/gettingstarted/AppConfig.java
@@ -1,13 +1,20 @@
 package gettingstarted;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import org.seasar.doma.SingletonConfig;
+import org.seasar.doma.jdbc.AbstractJdbcLogger;
 import org.seasar.doma.jdbc.Config;
+import org.seasar.doma.jdbc.JdbcLogger;
 import org.seasar.doma.jdbc.dialect.Dialect;
 import org.seasar.doma.jdbc.dialect.H2Dialect;
 import org.seasar.doma.jdbc.tx.LocalTransactionDataSource;
 import org.seasar.doma.jdbc.tx.LocalTransactionManager;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.util.function.Supplier;
 
 @SingletonConfig
 public class AppConfig implements Config {
@@ -40,5 +47,19 @@ public class AppConfig implements Config {
 
     public static AppConfig singleton() {
         return CONFIG;
+    }
+
+    @Override
+    public JdbcLogger getJdbcLogger() {
+        return new AbstractJdbcLogger<Level>(Level.INFO) {
+            @Override
+            protected void log(Level level, String callerClassName, String callerMethodName,
+                               Throwable throwable, Supplier<String> messageSupplier) {
+                LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+                Logger logger = loggerContext.getLogger(callerClassName);
+                int levelInt = Level.toLocationAwareLoggerInteger(level);
+                logger.log(null, callerClassName, levelInt, messageSupplier.get(), null, null);
+            }
+        };
     }
 }
