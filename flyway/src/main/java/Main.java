8a432bab07ac63b8851458f4ac135fd0ac61aa4f
irof <irof@hogedriven.net>
2015-09-29 22:14:49 +0900

    突っ込んだのselectしとく

--- flyway/src/main/java/Main.java
+++ flyway/src/main/java/Main.java
@@ -1,12 +1,21 @@
 import org.flywaydb.core.Flyway;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Flyway flyway = new Flyway();
 
-        flyway.setDataSource("jdbc:h2:mem:", "sa", null);
+        flyway.setDataSource("jdbc:h2:mem:hoge;DB_CLOSE_DELAY=-1", "sa", null);
 
         flyway.migrate();
+
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hoge", "sa", null);
+             Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery("select * from person")) {
+            while (resultSet.next())
+                System.out.println(resultSet.getString("name"));
+        }
     }
 }
