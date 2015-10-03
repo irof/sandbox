ddc91063ac031953c908bf210bd1eea279407e15
irof <irof@hogedriven.net>
2015-10-03 21:38:10 +0900

    接続エラーとか例外なると終了しないのでtry-with-resourceで終了させる
    ブログに書いてるやつ http://irof.hateblo.jp/entry/20130105/p1
    だけど close の throw Exceptin がそのまま生きてるので、
    投げられもしない Exception を処理しなきゃいけなくなるのがダサい。

--- quartz/src/main/java/jdbc/JobRegistrant.java
+++ quartz/src/main/java/jdbc/JobRegistrant.java
@@ -10,21 +10,21 @@ import org.quartz.impl.StdSchedulerFactory;
  * @author irof
  */
 public class JobRegistrant {
-    public static void main(String[] args) throws SchedulerException {
+    public static void main(String[] args) throws Exception {
         System.setProperty(StdSchedulerFactory.PROPERTIES_FILE,
                 ClassLoader.getSystemResource("jdbc/quartz.properties").getPath());
 
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-        scheduler.clear();
-        Trigger trigger1 = TriggerBuilder.newTrigger()
-                .withIdentity("myCronTrigger1", "myJdbcJobGroup")
-                .withSchedule(CronScheduleBuilder.cronSchedule("*/3 * * * * ?"))
-                .build();
-        scheduler.scheduleJob(JobBuilder.newJob(JdbcSlowJob.class)
-                .usingJobData("sleeps", 10L)
-                .storeDurably(true)
-                .build(), trigger1);
-
-        scheduler.shutdown();
+        try (AutoCloseable ac = scheduler::shutdown) {
+            scheduler.clear();
+            Trigger trigger1 = TriggerBuilder.newTrigger()
+                    .withIdentity("myCronTrigger1", "myJdbcJobGroup")
+                    .withSchedule(CronScheduleBuilder.cronSchedule("*/3 * * * * ?"))
+                    .build();
+            scheduler.scheduleJob(JobBuilder.newJob(JdbcSlowJob.class)
+                    .usingJobData("sleeps", 10L)
+                    .storeDurably(true)
+                    .build(), trigger1);
+        }
     }
 }
