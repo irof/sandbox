aed5e192412a3c406f4ed33210e4d0b77c472c07
irof <irof@hogedriven.net>
2015-10-07 01:06:35 +0900

    Spring+jdbcjobstore パラメタ引き継げてない

---
+++ quartz/src/test/java/spring/JdbcTest.java
@@ -0,0 +1,50 @@
+package spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author irof
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class JdbcTest {
+
+    @Test
+    public void test() throws Exception {
+        TimeUnit.SECONDS.sleep(20);
+    }
+
+    @Configuration
+    static class Config {
+
+        @Bean
+        public DataSource dataSource() {
+            return new DriverManagerDataSource("jdbc:hsqldb:hsql://localhost/", "SA", null);
+        }
+
+        @Bean
+        public SchedulerFactoryBean scheduler() {
+            SchedulerFactoryBean factory = new SchedulerFactoryBean();
+            factory.setDataSource(dataSource());
+            factory.setSchedulerName("MyJdbcScheduler");
+            Properties properties = new Properties();
+            properties.setProperty("org.quartz.jobStore.class", "org.quartz.impl.jdbcjobstore.JobStoreTX");
+            properties.setProperty("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.HSQLDBDelegate");
+            properties.setProperty("org.quartz.scheduler.instanceId", "AUTO");
+            properties.setProperty("org.quartz.jobStore.isClustered", "true");
+            factory.setQuartzProperties(properties);
+            return factory;
+        }
+    }
+}
