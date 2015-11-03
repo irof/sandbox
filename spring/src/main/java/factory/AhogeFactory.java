240d1aae420eea857b4da1d78f9bddf71ef71e2e
irof <irof@hogedriven.net>
2015-11-03 23:21:13 +0900

    なんとなくlambdaにしとく

--- spring/src/main/java/factory/AhogeFactory.java
+++ spring/src/main/java/factory/AhogeFactory.java
@@ -4,8 +4,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.FactoryBean;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -28,15 +26,12 @@ public class AhogeFactory<T> implements FactoryBean<T> {
     public T getObject() throws Exception {
         return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(),
                 new Class<?>[]{target},
-                new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        LOG.info("invoke method:" + method);
-                        // @Ahoge アノテーションに設定されている値をメソッドの戻り値とする。
-                        // なのでこの実装だと、戻り値はStringにしかできない。
-                        // 実用的には、戻り値型に応じて処理を振り分ける感じになる。
-                        return target.getAnnotation(Ahoge.class).value();
-                    }
+                (proxy, method, args) -> {
+                    LOG.info("invoke method:" + method);
+                    // @Ahoge アノテーションに設定されている値をメソッドの戻り値とする。
+                    // なのでこの実装だと、戻り値はStringにしかできない。
+                    // 実用的には、戻り値型に応じて処理を振り分ける感じになる。
+                    return target.getAnnotation(Ahoge.class).value();
                 });
     }
 
