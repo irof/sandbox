c9044f7316f306c438c7115368176da4110fefcb
irof <irof@hogedriven.net>
2015-11-05 09:19:58 +0900

    なんと無く繰り返し実行させてみる

--- spring-boot-batch/src/main/java/hello/BatchletConfiguration.java
+++ spring-boot-batch/src/main/java/hello/BatchletConfiguration.java
@@ -7,8 +7,12 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author irof
@@ -20,11 +24,17 @@ public class BatchletConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(BatchletConfiguration.class);
 
     @Bean
-    public Job batchletJob(JobBuilderFactory factory, StepBuilderFactory stepBuilderFactory) {
+    CountDownLatch latch() {
+        return new CountDownLatch(10);
+    }
+
+    @Bean
+    public Job batchletJob(JobBuilderFactory factory, StepBuilderFactory stepBuilderFactory, CountDownLatch latch) {
         TaskletStep step = stepBuilderFactory.get("taskletStep")
                 .tasklet((contribution, chunkContext) -> {
-                    logger.info("XXXXXX");
-                    return null;
+                    logger.info("XXXXXX; count={}", latch);
+                    latch.countDown();
+                    return RepeatStatus.continueIf(latch.getCount() > 0);
                 })
                 .build();
         return factory.get("batchletJob")
