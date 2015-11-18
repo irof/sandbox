071b06f2287201662f45a99cd61deb95a5213488
irof <irof@hogedriven.net>
2015-11-18 09:22:14 +0900

    IDEAでCDI使うときのおまじない

--- jbatch/src/main/java/sample/SimpleBatchlet.java
+++ jbatch/src/main/java/sample/SimpleBatchlet.java
@@ -3,6 +3,11 @@ package sample;
 import org.jberet.se.Main;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Named;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author irof
  */
@@ -27,6 +32,13 @@ public class SimpleBatchlet implements javax.batch.api.Batchlet {
         // 普通に実行する場合はこんな感じにする。
         // JobOperator jobOperator = BatchRuntime.getJobOperator();
         // jobOperator.start("XMLFILENAME", new Properties());
+
+        // weld-seをIDEAで動かすためのおまじない
+        Path beansXML = Paths.get("build/classes/main/META-INF/beans.xml");
+        if (!beansXML.toFile().exists()) {
+            Files.createDirectories(beansXML.getParent());
+            Files.createFile(beansXML);
+        }
 
         // けれど、SE環境で前述のように実行するとmainメソッドが即終了するので、
         // 実行完了まで待ち合わせてくれるのを使う。
