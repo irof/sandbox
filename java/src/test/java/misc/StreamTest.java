1b9770bc99e271f00f81a36d2976c7f979fb818f
irof <irof@hogedriven.net>
2015-11-29 18:41:48 +0900

    やりたいことをそのまま書いてみた

---
+++ java/src/test/java/misc/StreamTest.java
@@ -0,0 +1,56 @@
+package misc;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.junit.Assert.assertThat;
+
+/**
+ * オブジェクトのリストをインデックスつきの項目名でMapに詰める
+ *
+ * @author irof
+ */
+public class StreamTest {
+
+    @Test
+    public void flatMapしてentryをcollectしてみる() throws Exception {
+        List<TargetObject> objects = Arrays.asList(
+                new TargetObject("a1", "a2"),
+                new TargetObject("b1", "b2"));
+
+        Map<String, String> result = objects.stream()
+                .flatMap(t -> {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("key." + objects.indexOf(t) + ".hoge", t.hoge);
+                    map.put("key." + objects.indexOf(t) + ".fuga", t.fuga);
+                    return map.entrySet().stream();
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        assertThat(result, is(hasEntry("key.0.hoge", "a1")));
+        assertThat(result, is(hasEntry("key.0.hoge", "a1")));
+        assertThat(result, is(hasEntry("key.0.fuga", "a2")));
+        assertThat(result, is(hasEntry("key.1.hoge", "b1")));
+        assertThat(result, is(hasEntry("key.1.fuga", "b2")));
+    }
+
+    /**
+     * 対象のオブジェクト
+     */
+    static class TargetObject {
+        TargetObject(String hoge, String fuga) {
+            this.hoge = hoge;
+            this.fuga = fuga;
+        }
+
+        String hoge;
+        String fuga;
+    }
+}
