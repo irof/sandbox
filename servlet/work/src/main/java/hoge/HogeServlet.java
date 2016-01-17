cdf22da3221edbbc2d9797ef623a6ad33dbb426f
irof <irof@hogedriven.net>
2016-01-17 14:56:56 +0900

    fragmentの実験

---
+++ servlet/work/src/main/java/hoge/HogeServlet.java
@@ -0,0 +1,38 @@
+package hoge;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author irof
+ */
+public class HogeServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(HogeServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("doGet");
+        resp.getWriter().write("HELLO, " + this.getClass());
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        logger.info("init: {}", config);
+        super.init(config);
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("destroy");
+        super.destroy();
+    }
+}
