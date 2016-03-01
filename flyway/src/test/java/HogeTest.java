2efcf423455a6273dfd89ad07a71719b8d3346fc
irof <irof@hogedriven.net>
2016-03-01 09:18:38 +0900

    テスト時に別のロケーションを足す

--- flyway/src/test/java/HogeTest.java
+++ flyway/src/test/java/HogeTest.java
@@ -2,20 +2,32 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HogeTest {
 
     @Rule
-    public Migrator migrator = new Migrator();
+    public Migrator migrator = new Migrator("db/migration", "db/migration-test");
+
+    @Test
+    public void PERSONに3件入ってる() throws Exception {
+        // src/main/resources/db/migration が読まれていれば通る
+        try (Connection conn = migrator.getConnection();
+             Statement statement = conn.createStatement();
+             ResultSet rs = statement.executeQuery("select count(*) cnt from PERSON")) {
+            rs.next();
+            assertEquals(3, rs.getInt(1));
+        }
+    }
 
     @Test
     public void コミュニティテーブルに一件以上入っている() throws Exception {
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hoge", "sa", null);
+        // src/test/resources/db/migration-test が読まれていれば通る
+        try (Connection conn = migrator.getConnection();
              Statement statement = conn.createStatement();
              ResultSet rs = statement.executeQuery("select * from community")) {
             assertTrue(rs.next());
--- flyway/src/test/java/Migrator.java
+++ flyway/src/test/java/Migrator.java
@@ -1,17 +1,45 @@
 import org.flywaydb.core.Flyway;
-import org.junit.Before;
+import org.h2.jdbcx.JdbcConnectionPool;
 import org.junit.rules.ExternalResource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.StringJoiner;
 
 /**
  * @author irof
  */
 public class Migrator extends ExternalResource {
 
+    private JdbcConnectionPool dataSource;
+    private final String[] locations;
+
+    public Migrator(String... locations) {
+        this.locations = locations;
+    }
+
     @Override
     public void before() throws Exception {
+        String url = new StringJoiner(";")
+                .add("jdbc:h2:mem:hoge")
+                .add("DB_CLOSE_DELAY=-1")
+                .toString();
+        dataSource = JdbcConnectionPool.create(url, "sa", "sa");
+
         Flyway flyway = new Flyway();
-        flyway.setLocations("db/migration", "db/migration/test");
-        flyway.setDataSource("jdbc:h2:mem:hoge;DB_CLOSE_DELAY=-1", "sa", null);
+        flyway.setLocations(locations);
+        flyway.setDataSource(dataSource);
         flyway.migrate();
+    }
+
+    @Override
+    protected void after() {
+        if (dataSource == null) {
+            dataSource.dispose();
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 }
