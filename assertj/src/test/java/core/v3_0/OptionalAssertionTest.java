3fa8b4e53835304d7f17c184901416872537f2dd
irof <irof@hogedriven.net>
2016-08-03 00:17:54 +0900

    Optional検証の素振り

---
+++ assertj/src/test/java/core/v3_0/OptionalAssertionTest.java
@@ -0,0 +1,51 @@
+package core.v3_0;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Optionalの検証。
+ *
+ * `isNotPresent`は`isEmpty`のエイリアス、`isNotEmpty`は`isPresent`のエイリアスとしてv3.4.0で追加された。
+ * lambdaならではの `hasValueSatisfying` は、v3.4.0の`satisfies`メソッドに受け継がれていそう。
+ *
+ * @author irof
+ * @version assertj-core:3.0.0
+ * @see <a href="http://joel-costigliola.github.io/assertj/assertj-core-news.html#assertj-core-3.0.0-optional-assertions">Optinal assersions</a>
+ */
+public class OptionalAssertionTest {
+
+    @Test
+    public void 何も値が入っていない() throws Exception {
+        Optional<Object> empty = Optional.empty();
+
+        assertThat(empty)
+                // isEmpty = isNotPresent
+                .isNotPresent()
+                .isEmpty();
+    }
+
+    @Test
+    public void 何か値が入っている() throws Exception {
+        Optional<String> opt = Optional.of("hoge");
+
+        assertThat(opt)
+                // isPresent = isNotEmpty
+                .isPresent()
+                .isNotEmpty()
+                // contains = hasValue
+                .contains("hoge")
+                .hasValue("hoge");
+
+        // 値に対する一致以外の検証は `hasValueSatisfying` を使用する。
+        // ネストになるので少々読みづらい。
+        assertThat(opt).hasValueSatisfying(value ->
+                assertThat(value)
+                        .startsWith("hog")
+                        .endsWith("oge")
+        );
+    }
+}
