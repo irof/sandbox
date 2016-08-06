61e248834232ac90ae3109637093d80062f2dcc9
irof <irof@hogedriven.net>
2016-08-06 22:52:55 +0900

    NavigateAndPerformAssertionsの素振り

--- assertj/src/test/java/core/v3_5/NavigateAndPerformAssertions.java
+++ assertj/src/test/java/core/v3_5/NavigateAndPerformAssertions.java
@@ -1,24 +1,50 @@
 package core.v3_5;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.StringAssert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * 集合の個々の要素に対する検証をチェーンできるようにするもの。
  *
- * Iterableの各要素と、IterableおよびMapのサイズへのナビゲートが追加された。
+ * Iterableの各要素へのナビゲートと、IterableおよびMapのサイズへのナビゲートと戻しが追加された。
+ * この機能を使わないとAssertを1メソッドで完結させられない。そこにモチベーションがあればって感じ。
  *
  * @author irof
- * @version assertj-core:3.5.0
+ * @version 2.5.0 / 3.5.0
+ * @see <a href="http://joel-costigliola.github.io/assertj/assertj-core-news.html#assertj-core-2.5.0-navigational-assertions-element">Navigate and perform assertions on an Iterable's elements</a>
  */
 public class NavigateAndPerformAssertions {
 
     @Test
-    public void MapSizeAssertの素振り() throws Exception {
-        Map<String, Object> map = new HashMap<>();
+    public void Iterableの要素に対する検証への切り替えができる() throws Exception {
+        List<String> list = Arrays.asList("Hoge", "Fuga", "Piyo");
+
+        Assertions.assertThat(list, StringAssert.class)
+                .first()
+                .startsWith("Ho");
+
+        // 第二引数に取り出した要素に使用したいAssertを指定しないとObjectAssertになり、最低限の検証しか行えない。
+        Assertions.assertThat(list)
+                .last()
+                .isEqualTo("Piyo");
+
+        // 出番があるとしたら、リストに対する何かしらの検証を行ったあと、
+        // 続けて要素のうち代表に対する検証を行いたい場合かな。
+        Assertions.assertThat(list, StringAssert.class)
+                .contains("Hoge")
+                .element(1)
+                .endsWith("uga");
+    }
+
+    @Test
+    public void Mapのサイズに対する検証への切り替えができる() throws Exception {
+        Map<String, String> map = new HashMap<>();
         map.put("a", "hoge");
         map.put("b", "fuga");
         map.put("c", "piyo");
@@ -37,5 +63,12 @@ public class NavigateAndPerformAssertions {
                 .returnToMap()
                 // 他の検証を続けたければ繋げる。
                 .containsValue("fuga");
+
+        // first/last/elementのようなものは追加されていない。
+        // entrySetに対して行えば可能だが、Map.Entryをアサーションするのも面倒な感じがする。
+        // と思いつつ書いたら、Map.EntryのAssertがないのでObjectAssertになって超微妙。
+        Assertions.assertThat(map.entrySet())
+                .last()
+                .matches(entry -> entry.getValue().length() == 4);
     }
 }
