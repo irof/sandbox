4265966960906a65b50242685c480baf5dea8b75
irof <irof@hogedriven.net>
2017-01-10 20:55:00 +0900

    とりあえずのindex表示

---
+++ spring-boot/src/main/java/fuga/FugaApplication.java
@@ -0,0 +1,15 @@
+package fuga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author irof
+ */
+@SpringBootApplication
+public class FugaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FugaApplication.class, args);
+    }
+}
---
+++ spring-boot/src/main/java/fuga/view/IndexController.java
@@ -0,0 +1,19 @@
+package fuga.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author irof
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "hoge");
+        model.addAttribute("value", "fuga");
+        return "index";
+    }
+}
