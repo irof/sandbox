f59134b9f4f6705d59439b2c5d03b7ff2706e9b2
irof <irof@hogedriven.net>
2017-01-11 21:58:36 +0900

    thymeleafでLocalDateTimeのフォーマット

--- spring-boot/src/main/java/fuga/spring/mvc/SecretController.java
+++ spring-boot/src/main/java/fuga/spring/mvc/SecretController.java
@@ -1,5 +1,8 @@
 package fuga.spring.mvc;
 
+import fuga.domain.Account;
+import fuga.spring.security.MyUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SecretController {
 
     @RequestMapping("/user")
-    public String user(Model model) {
+    public String user(Model model, @AuthenticationPrincipal MyUserDetails details) {
+
+        Account account = Account.builder()
+                .username(details.getUsername())
+                .mailAddress(details.getAccount().getMailAddress())
+                .createdDate(details.getAccount().getCreatedDate())
+                .build();
+        model.addAttribute(account);
+
         return "secret/user";
     }
 }
