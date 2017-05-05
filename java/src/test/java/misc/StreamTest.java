8155da01e7c408bdecd28eb330ee42b3c3141b18
irof <irof@hogedriven.net>
2017-05-05 15:44:38 +0900

    Streamで遊ぶやつ

---
+++ java/src/test/java/misc/StreamTest.java
@@ -0,0 +1,24 @@
+package misc;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+public class StreamTest {
+
+    @Test
+    public void test() throws Exception {
+        Stream.of("abc", "aaa1", "aaa2", "aaa3", "aaa4", "aaa5", "aaa6")
+                //.parallel()
+                .map(str -> {
+                    System.out.println("toUpperCase:" + str);
+                    return str.toUpperCase();
+                })
+                .filter(str -> {
+                    System.out.println("filter:" + str);
+                    return str.length() == 4;
+                })
+                .findAny()
+                .ifPresent(System.out::println);
+    }
+}
