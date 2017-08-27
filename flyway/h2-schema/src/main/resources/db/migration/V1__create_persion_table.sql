4f20ec47bde433d570dc974efffc4332a9d1c791
irof <irof@hogedriven.net>
2017-08-27 15:59:27 +0900

    h2でschemaを扱うときは大文字にする

---
+++ flyway/h2-schema/src/main/resources/db/migration/V1__create_persion_table.sql
@@ -0,0 +1,6 @@
+create schema if not exists hoge;
+
+create table hoge.PERSON (
+    ID int not null,
+    NAME varchar(100) not null
+);
---
+++ flyway/h2-schema/src/main/resources/db/migration/V2__create_team_table.sql
@@ -0,0 +1,6 @@
+create schema if not exists fuga;
+
+create table fuga.team(
+    ID int not null,
+    NAME varchar(100) not null
+);
