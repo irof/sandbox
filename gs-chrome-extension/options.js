8ae21e852c2cc088a0c440bca996cdd1ced43bbe
irof <irof@hogedriven.net>
2018-04-03 16:36:33 +0900

    pageって言ってるけどbuttonDivだろ、とおもってやったらできた

--- gs-chrome-extension/options.js
+++ gs-chrome-extension/options.js
@@ -1,6 +1,7 @@
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1']
 
 function constructOptions(kButtonColors) {
+  let page = document.getElementById('buttonDiv');
   for (let item of kButtonColors) {
     let button = document.createElement('button');
     button.style.backgroundColor = item;
